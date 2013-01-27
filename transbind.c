cd0183ea9a9fe6d99f8a9ac38f6d51561363723f
Cyril Bonté <cyril.bonte@free.fr>
2013-01-27 20:47:54 +0100

    Add TRANSBIND_VERBOSITY environment variable to enable/disable debug messages

--- transbind.c
+++ transbind.c
@@ -1,6 +1,7 @@
 #include <sys/syscall.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -12,31 +13,40 @@
 
 int bind(int s, const struct sockaddr *sa, socklen_t len)
 {
+	char *verbosity_env;
+	int verbosity;
 	unsigned one = 1;
 	int success = 0;
 	char straddr[INET6_ADDRSTRLEN];
-	
+
+	verbosity_env = getenv("TRANSBIND_VERBOSITY");
+	verbosity = verbosity_env ? atoi(verbosity_env) : 0;
+
 	switch (sa->sa_family) {
 		case AF_INET:
 			if ((setsockopt(s, SOL_IP, IP_TRANSPARENT, &one, sizeof(one)) == 0)
 			    || (setsockopt(s, SOL_IP, IP_FREEBIND, &one, sizeof(one)) == 0)) {
 				success = 1;
 			}
-			fprintf(stderr, "Enabling transparent bind on %s:%d : %s\n",
-				inet_ntop(AF_INET, &((struct sockaddr_in*)sa)->sin_addr, straddr, sizeof(straddr)),
-				ntohs(((struct sockaddr_in*)sa)->sin_port),
-				success ? "success" : "failed"
-      			);
+			if (verbosity > 0) {
+				fprintf(stderr, "Enabling transparent bind on %s:%d : %s\n",
+					inet_ntop(AF_INET, &((struct sockaddr_in*)sa)->sin_addr, straddr, sizeof(straddr)),
+					ntohs(((struct sockaddr_in*)sa)->sin_port),
+					success ? "success" : "failed"
+				);
+			}
 		break;
 		case AF_INET6:
 			if (setsockopt(s, SOL_IPV6, IPV6_TRANSPARENT, &one, sizeof(one)) == 0) {
 				success = 1;
 			}
-			fprintf(stderr, "Enabling transparent bind on [%s]:%d : %s\n",
-				inet_ntop(AF_INET6, &((struct sockaddr_in6*)sa)->sin6_addr, straddr, sizeof(straddr)),
-				ntohs(((struct sockaddr_in6*)sa)->sin6_port),
-				success ? "success" : "failed"
-      			);
+			if (verbosity > 0) {
+				fprintf(stderr, "Enabling transparent bind on [%s]:%d : %s\n",
+					inet_ntop(AF_INET6, &((struct sockaddr_in6*)sa)->sin6_addr, straddr, sizeof(straddr)),
+					ntohs(((struct sockaddr_in6*)sa)->sin6_port),
+					success ? "success" : "failed"
+				);
+			}
 		break;
 	}
 	return syscall(SYS_bind, s, sa, len);
